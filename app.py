79e199fba3807531a6c53fbf3f12764b33d39cd0
nancyerika04 <nancywouegam@gmail.com>
2025-01-10 21:44:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ model = load_my_model()
 
 # Fonction de prédiction
 def predict(image, model):
-    image = ImageOps.fit(image, (224, 224))  # Ajuster la taille
-    image_array = np.array(image).astype('float32') / 255.0  # Normaliser l'image
+    image = ImageOps.fit(image, (224, 224))  
+    image_array = np.array(image).astype('float32') / 255.0  
 
     # Si l'image n'a pas de canal de couleur, ajoutez-en un
     if len(image_array.shape) == 2:  # Image en niveaux de gris
-        image_array = np.expand_dims(image_array, axis=-1)  # Ajouter un canal
-        image_array = np.repeat(image_array, 3, axis=-1)  # Répéter le canal pour en avoir 3 (RGB)
+        image_array = np.expand_dims(image_array, axis=-1)  
+        image_array = np.repeat(image_array, 3, axis=-1)  
 
-    image_array = np.expand_dims(image_array, axis=0)  # Ajouter une dimension batch
+    image_array = np.expand_dims(image_array, axis=0)  
 
     prediction = model.predict(image_array)
-    return prediction[0][0]  # Retourne la probabilité
+    return prediction[0][0]  
 
 # Interface utilisateur Streamlit
 st.title("Détection de fractures de l'humérus")
